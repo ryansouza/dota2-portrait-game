cfa3a6eaa62fe2875384f61a01fccb97966ae965
Joseph Hsu <jhsu.x1@gmail.com>
2014-09-11 23:56:40 -0700

    move the initial random hero select to a property

--- js/controllers.js
+++ js/controllers.js
@@ -1,17 +1,22 @@
-PortraitGame.PortraitsController = Ember.ObjectController.extend({
+PortraitGame.PortraitsController = Ember.ArrayController.extend({
   picked: null,
   revealed: false,
+  hero: function() {
+    return this.sample();
+  }.property(),
+  sample: function() {
+    var i = Math.floor(Math.random() * this.get('model.length'));
+    return this.objectAt(i);
+  },
   actions: {
     // Reveal the name of the displayed hero
     reveal: function() {
       this.set('revealed', true);
     },
-
     // Display a new random hero
     next_hero: function() {
-      var i = Math.floor(Math.random() * this.get('model').content.length);
-      this.set('picked', this.get('model').objectAt(i));
       this.set('revealed', false);
+      this.set('hero', this.sample());
     }
   }
 });
